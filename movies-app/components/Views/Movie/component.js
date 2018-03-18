6e26a215976f046d44e883932e1e4f249747470d
feliun <felipe.polo@gmail.com>
2018-03-18 18:25:50 +0100

    First movies rendering & styling

--- movies-app/components/Views/Movie/component.js
+++ movies-app/components/Views/Movie/component.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-const MoviesList = ({ movie }) => (
-  <div>About to display {movie.title}...</div>
+const Movie = ({ movie }) => (
+  <div>
+    <style>{
+        `
+        .movie {
+          width: 100px;
+        }
+        `
+      }
+    </style>
+    <section className="item col-xs-12 col-sm-6 col-md-4 all category2 category5">
+      <a href="assets/images/1.jpg" data-fluidbox>
+        <img src={movie.images.poster} alt="" className="img-responsive movie" />
+      </a>
+    </section>
+  </div>
 );
 
-export default MoviesList;
+export default Movie;
--- movies-app/components/Views/MoviesList/component.js
+++ movies-app/components/Views/MoviesList/component.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import Movie from '../Movie/container';
+import './moviesList.css';
 
 const MoviesList = ({ movies = [] }) => (
   <div>
-    {
-      movies.map((movie) => <Movie key={movie.id} movie={movie} />)
-    }
+    <style>{
+        `
+        #movie-list {
+          background-color: black;
+        }
+        .row {
+          display: -webkit-box;
+          display: -ms-flexbox;
+          display: flex;
+          -ms-flex-wrap: wrap;
+          flex-wrap: wrap;
+          margin-right: -15px;
+          margin-left: -15px;
+        }
+        `
+      }
+    </style>
+    <section id="movie-list" className="row">
+      {
+        movies.map((movie) => <Movie key={movie.id} movie={movie} />)
+      }
+    </section>
   </div>
 );
 
