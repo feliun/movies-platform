c731ceadd69979931c0689307731f1c130da48d2
feliun <felipe.polo@gmail.com>
2018-03-20 17:41:06 +0100

    More movie detail

--- movies-app/components/Views/MovieDetail/component.js
+++ movies-app/components/Views/MovieDetail/component.js
@@ -1,6 +1,7 @@
+import moment from 'moment';
 import React from 'react';
 
-const MovieDetail = ({ title, synopsis, year, images, rating, runtime, released, trailer, genres }) => (
+const MovieDetail = ({ id, title, synopsis, year, images, rating, runtime, released, trailer, genres }) => (
   <div>
     <style>{
       `
@@ -13,14 +14,34 @@ const MovieDetail = ({ title, synopsis, year, images, rating, runtime, released,
         border: 1px solid gray;
         border-radius: 5px;
       }
+      #imdb-link-img {
+        width: 20px;
+      }
+      #title {
+        padding-left: 0px;
+      }
       h1 {
-        font-size: 20px;
+        font-size: 25px;
         color: white;
       }
+      #year {
+        font-size: 15px;
+        color: gray;
+      }
+      #rating {
+        font-size: 18px;
+      }
+      #imdb-score-img {
+        width: 15px;
+      }
       p {
-        font-size: 12px;
+        font-size: 10px;
         color: white;
         text-align: justify;
+      }
+      #movie-details-info {
+        color: gray;
+        font-size: 9px;
       }
       `
     }
@@ -31,27 +52,25 @@ const MovieDetail = ({ title, synopsis, year, images, rating, runtime, released,
       </div>
       <div className="col-8">
         <div className="row">
+          <div className="col-1">
+            <a href={`http://www.imdb.com/title/${id}`} target="_blank">
+              <img id="imdb-link-img" alt="imdb" src="https://images-na.ssl-images-amazon.com/images/G/01/imdb/images/logos/imdb_fb_logo-1730868325._CB514892123_.png" />
+            </a>
+          </div>
+          <div id="title" className="col-11">
+            <h1>
+              {title}&nbsp;
+              <span id="year">({year})</span>
+              <span id="rating">
+                <img id="imdb-score-img" alt="score" src="http://icons.iconarchive.com/icons/aha-soft/multimedia/256/favourites-star-SH-icon.png" />
+                {rating.percentage / 10 }/10
+              </span>
+            </h1>
+          </div>
+        </div>
+        <div className="row">
           <div className="col-12">
-            <h1>{title}</h1>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-1">
-            <p>{year}</p>
-          </div>
-          <div className="col-2">
-            <p>{runtime} min</p>
-          </div>
-          <div className="col-1">
-            <p>{rating.percentage}</p>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-1">
-            <p>{genres[0]}</p>
-          </div>
-          <div className="col-1">
-            <p>{genres[1]}</p>
+            <p id="movie-details-info">{runtime} min | {genres.join(', ')} | {moment(released * 1000).format('DD MMMM YYYY')}</p>
           </div>
         </div>
         <div className="row">
